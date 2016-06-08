ac09002cbdcbe0b75d1dbb47b82dd30706b3ceaf
Lionel T <elrumordelaluz@hotmail.com>
2016-06-08 16:07:45 +0200

    Add babel-cli. Update structure

--- src/index.js
+++ src/index.js
@@ -1,8 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import App from './components/App'
-
-ReactDOM.render(
-  <App />, 
-document.querySelector('.container'));
+export ColorPicker from './components/ColorPicker';
