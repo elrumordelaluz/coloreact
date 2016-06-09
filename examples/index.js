1454a3bf47f4352b8a9d304047963f62ff22a117
Lionel T <elrumordelaluz@hotmail.com>
2016-06-09 17:51:25 +0200

    Update example

--- examples/index.js
+++ examples/index.js
@@ -4,13 +4,36 @@ import ReactDOM from 'react-dom';
 import { ColorPicker } from '../src';
 
 class App extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      currentColor: '#f00'
+    }
+  }
+
+  handleChange (color) {
+    this.setState({
+      currentColor: color
+    })
+  }
   render () {
     return (
-      <ColorPicker
-        opacity={true}
-        color="rgba(25,6,1,1.5)"
-        onChange={color => color }
-        onComplete={color => console.log(color) } />
+      <div>
+        <button onClick={() => this.handleChange('#d5d5d5')}>a</button>
+        <button onClick={() => this.handleChange('#0f34a1')}>b</button>
+        <button onClick={() => this.handleChange('#7a3')}>c</button>
+        <div style={{
+            position: 'relative',
+            width: '100%',
+            height: '400px',
+          }}>
+          <ColorPicker
+            opacity={true}
+            color={this.state.currentColor}
+            onChange={color => color }
+            onComplete={color => console.log(color) } />
+        </div>
+      </div>
     );
   }
 }
