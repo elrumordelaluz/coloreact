48a69635dba2fd8800eda587c87a9be2ccd3f8f4
Lionel T <elrumordelaluz@hotmail.com>
2016-06-12 12:55:49 +0200

    Change export default

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-export ColorPicker from './components/ColorPicker';
+import ColorPicker from './components/ColorPicker';
 export Slider from './components/Slider';
 export Map from './components/Map';
+
+export default ColorPicker;
